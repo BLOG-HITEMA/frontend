c60c09ff82ccdf24d6e3073498c5706a1b5e589c
Loick Hommet <hommet.loick@gmail.com>
2022-06-27 16:19:34 +0200

    Ajoue css dans app.js

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style>
-
+@import "bootstrap/dist/css/bootstrap.min.css";
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap');
+@import '@/assets/style.css';
 </style>
