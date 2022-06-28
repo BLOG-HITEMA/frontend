b94bc55b6980344d9fe92fa5904185b4a95c481c
wezerg <vivian.grimaux@hotmail.fr>
2022-06-28 08:18:56 +0200

    Initialisation : ajout du header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { useAuthService } from '../services/authService';
+import Button from './Button.vue';
+
+const {auth, disconnect} = useAuthService();
+</script>
+<template>
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <RouterLink class="navbar-brand" to="/">Accueil</RouterLink>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item" v-if="auth">
+                <RouterLink class="nav-link active" to="/testauth">Test page with Auth Guard</RouterLink>
+            </li>
+            <li class="nav-item" v-if="!auth">
+                <RouterLink class="nav-link" to="/test">Test page without Auth Guard</RouterLink>
+            </li>
+        </ul>
+        <Button v-if="auth" :classSup="'ms-auto'" :text="'Déconnexion'" :click="disconnect"/>
+    </div>
+</nav>
+</template>
