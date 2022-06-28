8a9349f950ba69f1ea9c3d5c1347effb34437871
wezerg <vivian.grimaux@hotmail.fr>
2022-06-28 14:16:22 +0200

    Premier layout de l'Aside Ajout de l'aside dans l'app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
+import Aside from './components/Aside.vue';
 </script>
 
 <template>
-    <Header/>
-    <RouterView />
+    <Aside />
+    <div class="container-custom">
+        <Header />
+        <RouterView />
+    </div>
 </template>
 
 <style>
 @import "bootstrap/dist/css/bootstrap.min.css";
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap');
 @import '@/assets/style.css';
+.container-custom{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    height: 100%;
+    max-height: 100%;
+}
 </style>
---
+++ src/components/Aside.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+import { useAuthService } from '../services/authService';
+
+const {auth, disconnect} = useAuthService();
+
+    const active = ref(true);
+</script>
+<template>
+    <aside :class="active ? 'no-collapsed' : 'collapsed'">
+        <nav>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <RouterLink class="nav-link active" to="/testauth">Accueil</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/test">Recherche</RouterLink>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+</template>
+<style scoped>
+aside{
+    min-width: 175px;
+    width: 175px;
+    max-width: 175px;
+}
+.no-collapsed {
+    transition: all 0.3s ease;
+    margin-left: 0;
+}
+.collapsed {
+    transition: all 0.3s ease;
+    margin-left: -175px;
+}
+</style>
