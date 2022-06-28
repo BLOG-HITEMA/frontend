a84bed840c28da046021e7f210d7abd2a163b978
Loick Hommet <hommet.loick@gmail.com>
2022-06-28 11:33:06 +0200

    création des lien login/register dans le Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,8 +20,8 @@ const {auth, disconnect} = useAuthService();
                 <RouterLink class="nav-link" data-test-profil='loick' :to="`/user/${auth.id}`">{{auth.name}}</RouterLink>
             </li>
         </ul>
-        
-        <RouterLink v-if="!auth" class="me-2 buttonLien" to="/login">Connexion</RouterLink>
+
+        <RouterLink v-if="!auth" class="me-2 buttonLien" data-test-conneted='conneted' to="/login">Connexion</RouterLink>
         <RouterLink v-if="!auth" class=" buttonLien" to="/register">Inscription</RouterLink>
         
         <Button v-if="auth" :classSup="'ms-auto'" :text="'Déconnexion'" :click="disconnect"/>
