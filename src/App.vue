5831dc417b86a975f77e0dc50d91f4a535f1dbf6
wezerg <vivian.grimaux@hotmail.fr>
2022-06-28 08:20:18 +0200

    Initialisation : modification de l'app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterView } from 'vue-router';
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  
-
-  <RouterView />
+    <Header/>
+    <RouterView />
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import Toast, { POSITION, TYPE } from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+const options = {
+    position: POSITION.TOP_RIGHT,
+    type: TYPE.DEFAULT,
+};
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+app.use(Toast, options)
 
 app.mount('#app')
