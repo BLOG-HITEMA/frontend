74d78a4172b4cd0165a8c07ab5b4f500e75fea58
wezerg <vivian.grimaux@hotmail.fr>
2022-06-28 19:52:14 +0200

    Refactorisation des pages login et Register Utilisation du service toast dans le service d'auth Correction graphique

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,76 +8,33 @@ import { useToastService } from "../services/toastService";
 import Joi from "joi";
 import { ref } from "vue";
 
-const {errorToast, successToast} = useToastService();
-
-const { connect, auth } = useAuthService();
-const router = useRouter();
+const { connect } = useAuthService();
 
 const email = ref("");
 const password = ref("");
-const erroremail = ref('');
-const errorpassword = ref('');
-
-// envoie un toast quand je recois une erreur et quand je me connect
-/*const toast = useToast();
-function displayToast(msg, type = "info"){
-    switch (type) {
-        case "info":
-            toast.info(msg);
-            break;
-        case "success":
-            toast.success(msg);
-            break;
-        case "error":
-            toast.error(msg);
-            break;
-        case "warning":
-            toast.warning(msg);
-            break;
-        default:
-            toast.default(msg);
-            break;
-    }
-}*/
+const errorEmail = ref('');
+const errorPassword = ref('');
 
 // Fonction de connexion
 async function connexion() {
-
     //Shema a réspérter
-  const scheme = Joi.object({
-    email: Joi.string().required(),
-    password: Joi.string().required()
-  });
-  const payload = { email: email.value, password: password.value };
-  const { value, error } = scheme.validate(payload, {abortEarly: false});
-
+    const scheme = Joi.object({
+        email: Joi.string().required(),
+        password: Joi.string().required()
+    });
+    const payload = { email: email.value, password: password.value };
+    const { value, error } = scheme.validate(payload, {abortEarly: false});
     // Identifier une erreur dans les champs eamil et password 
-  if (error) {
-    
-    for (let index = 0; index < error.details.length; index++) {
-        if(error.details[index].context.key == 'email'){
-            erroremail.value = error.details[index].message
-            console.log(erroremail.value, ' Email')
-        }
-        if(error.details[index].context.key == 'password'){
-            errorpassword.value = error.details[index].message
-            console.log(errorpassword.value)
-        }
-        
-    }
-    
-    return;
-  } else {
-    // Fonction pour ce connecter
-    const response = await connect(email.value, password.value);
-    if (!response) {   
-      errorToast("Cette identifiant n'exite pas");
-      return null;
+    if (error) {
+        errorEmail.value = error.details.find(elm => elm.path[0] === "email")?.message ?? "";
+        errorPassword.value = error.details.find(elm => elm.path[0] === "password")?.message ?? "";
+        return;
     } else {
-      successToast("Vous êtes connecté");
-      router.push({ name: "home" });
+        // Fonction pour ce connecter
+        errorEmail.value = "";
+        errorPassword.value = "";
+        const response = await connect(email.value, password.value);
     }
-  }
 }
 
 </script>
@@ -87,12 +44,11 @@ async function connexion() {
       <form @submit.prevent="connexion()" class="col-12 col-lg-6 col-md-8">
         <h1 class="title mb-0 my-5" data-test-h1="h1-login">Login</h1>
         <div class="form-group mb-2 my-4">
-          <Input :placeholder="'Email'" :error="erroremail" :type="'email'" v-model="email" />
+          <Input :placeholder="'Email'" :error="errorEmail" :type="'email'" v-model="email" />
         </div>
         <div class="form-group mb-2 my-4">
-          <Input :placeholder="'Mot de passe'" :error="errorpassword" :type="'password'" v-model="password" />
+          <Input :placeholder="'Mot de passe'" :error="errorPassword" :type="'password'" v-model="password" />
         </div>
-
         <Button :type="'submit'" :text="'Connexion'" :classSup="'w-100 mb-3'" />
       </form>
     </div>
