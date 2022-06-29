1c32eef3ad0dd5a902d1fb5c606f60c8576ac081
wezerg <vivian.grimaux@hotmail.fr>
2022-06-29 16:18:23 +0200

    Style TextArea

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -17,7 +17,7 @@
         },
         rows: {
             type: Number,
-            default: 5
+            default: 15
         },
         cols: {
             type: Number,
@@ -42,5 +42,16 @@
     <p class="mb-3 text-red" v-if="error">{{error}}</p>  
 </template>
 <style scoped>
-
+textarea{
+    resize: none;
+    transition: all 0.15s ease-in-out;
+    border-radius: 5px;
+    border: 1px solid rgba(var(--grey-color));
+    padding: 10px 24px;
+    box-shadow: 0px 0px 5px 2px rgba(var(--grey-color), 0.5);
+}
+textarea:hover{
+    transition: all 0.15s ease-in-out;
+    box-shadow: 0px 0px 5px 2px rgba(var(--blue-color), 0.4);
+}
 </style>
