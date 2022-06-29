33438cff21bd3f0112f0c38dbb5d97d69f8999ac
wezerg <vivian.grimaux@hotmail.fr>
2022-06-29 16:33:20 +0200

    Correction graphique Suppression de la page test et ses ancres Mise à jour du header et de l'aside

---
+++ src/views/Search.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Input from '@/components/Input.vue';
+
+</script>
+<template>
+    <main class="container-fluid">
+        <div class="row justify-content-center">
+            <form class="col-12 col-lg-8" action="">
+                <Input/>
+            </form>
+        </div>
+    </main>
+</template>
+<style scoped>
+
+</style>
