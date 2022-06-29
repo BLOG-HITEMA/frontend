933280edb8a8181955267e6c312e1c1369351f38
Loick Hommet <hommet.loick@gmail.com>
2022-06-29 15:29:04 +0200

    Ajoue vue details

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,11 +1,51 @@
 <script setup>
+import { useArticleService } from "@/services/articleService";
+import { onMounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const { getById } = useArticleService();
+
+const { id } = router.currentRoute.value.params;
+
+const article = ref(null);
+
+onMounted(async () => {
+  const response = await getById(id);
+  article.value = response;
+});
 
 </script>
 <template>
-    <main>
-        Article
-    </main>
+  <main>
+    
+    <div v-if="article">
+    <section
+      id="banner"
+      :style="`background-image:url(${article.image}); background-size:cover;`"
+    >
+    </section>
+    <div class="container-fluid">
+       <div class="row justify-content-center">
+        <header>
+          <h1> {{article.title}}</h1>
+
+          <p> {{article.content}}</p>
+        </header>
+      </div>
+    </div>
+  </div>
+  </main>
 </template>
 <style scoped>
+#banner{
+    	padding: 12em 0 0 0;
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		text-align: center;
+		position: relative;
+}
 
 </style>
