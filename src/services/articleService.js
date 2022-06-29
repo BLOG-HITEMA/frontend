a11d7bce73f5687c5da60859013945787cf9e5ee
wezerg <vivian.grimaux@hotmail.fr>
2022-06-29 12:10:31 +0200

    Correction home

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -6,32 +6,30 @@ import { useRouter } from "vue-router";
 const {errorToast, successToast} = useToastService();
 const router = useRouter();
 
-/*
+
 axios.interceptors.request.use(function (config) {
-    config.headers['x-auth-token'] = localStorage.getItem('token-projectm1') || "";
+    config.headers['Authorization'] = `Bearer ${localStorage.getItem('token-projectm1')}` || "";
     return config;
 });
 
 axios.interceptors.response.use(function (response) {
     return response;
-});*/
+});
 
-const article = ref(null);
+const articles = ref(null);
 
 function useArticleService(){
-    return {article, getAll};
+    return {articles, getAll};
 }
 
-async function getAll(){
+async function getAll(){ // A FINIR --------------------
     const response = await axios.get('http://localhost:3001/api/articles/all/1').then(res => res).catch(err => err);
     if (response.status !== 200) {
+        errorToast("Une erreur est survenue");
         return null;
     }
-    console.log(response.data)
-    article.value = response.data;
+    articles.value = response.data;
     return response;
 }
-
-
 
 export {useArticleService};
